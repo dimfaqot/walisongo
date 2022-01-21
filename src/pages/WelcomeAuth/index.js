7cb2d71e98bf5b18eebf736a52c38bbe7e48c5b4
Dimfaqot <dimyfaqot@gmail.com>
2022-01-22 01:13:45 +0700

    Menambah Navigation Stack dan Halaman

---
+++ src/pages/WelcomeAuth/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const WelcomeAuth = () => {
+  return (
+    <View>
+      <Text>Welcome Auth</Text>
+    </View>
+  );
+};
+
+export default WelcomeAuth;
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Splash, WelcomeAuth, Register, Login} from '../pages';
+
+const Stack = createNativeStackNavigator();
+const Router = () => {
+  return (
+   <Stack.Navigator>
+       <Stack.Screen name="Splash" component={Splash} />
+       <Stack.Screen name="Login" component={Login} />
+       <Stack.Screen name="Register" component={Register} />
+       <Stack.Screen name="WelcomeAuth" component={WelcomeAuth} />
+   </Stack.Navigator>
+  );
+};
+
+export default Router;
